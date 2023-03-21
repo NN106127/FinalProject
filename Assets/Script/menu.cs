090b24a21412857a919aab05a114b52b7526c7e2
specteruwu <98225008+specteruwu@users.noreply.github.com>
2023-03-21 16:37:41 +0800

    menu

---
+++ Assets/Script/menu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScence(string scencename)
+    {
+        //m_audio.Play();
+        SceneManager.LoadScene(1);
+        SceneManager.LoadSceneAsync(scencename);
+    }
+
+    public void QuitGame()
+    {
+        //m_audio.Play();
+        Application.Quit();
+    }
+}
