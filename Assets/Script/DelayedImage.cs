872c95a19e91ab26adbaba9ae14831144afe16af
specteruwu <98225008+specteruwu@users.noreply.github.com>
2023-04-22 19:02:33 +0800

    隱藏袋子(尚未修正)

---
+++ Assets/Script/DelayedImage.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayedImage : MonoBehaviour
+{
+    public float delayTime = 1.5f; // 延迟时间
+    public GameObject bone;
+    public GameObject ice;
+
+
+    void Start()
+    {
+        ice.SetActive(false);
+        bone.SetActive(false);
+        
+    }
+    void Update()
+    {
+        if (ice.activeSelf == true)
+        {
+            
+            Invoke("ShowImage", delayTime); // 延迟调用 ShowImage 方法
+        }
+        else
+        {
+            bone.SetActive(false);
+        }
+    }
+    void ShowImage()
+    {
+        bone.SetActive(true);
+        
+    }
+   
+
+}
